93319c50f27df43155121533bfd6cdc5b3e00ffa
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-26 21:10:18 +0100

    .env changes

--- Server/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    MONGODB_URI:"mongodb+srv://admin:SkgCffMezqG6BxXX@cluster0.dvexo.mongodb.net/WebAppDatabase?retryWrites=true&w=majority"
-}
--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Cors = require('cors')
-const {MONGODB_URI} = require('./config/keys.js')
 const app = express()
 const API_PORT = process.env.API_PORT || 5000
 
 //----Database connection
-mongoose.connect(MONGODB_URI,{
+mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useFindAndModify: false
 })
-mongoose.connection.on('connected',()=>{
+const db = mongoose.connection
+db.once('open',()=>{
     console.log("Connected to database")
 })
-mongoose.connection.on('error',(err)=>{
+db.on('error',(err)=>{
     console.log("Error connecting to database. ",err)
 })
 //----
