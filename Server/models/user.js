cd0ad365d3d16d4d4ddf70a24680b5f29a46cc7e
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-27 00:37:08 +0100

    Fixed contacts id type

--- Server/models/user.js
+++ Server/models/user.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
         required:true
     },
     contacts:[{
-        type: ObjectId,
+        type: mongoose.ObjectId,
         ref: 'Contact'
     }],
     token:{
