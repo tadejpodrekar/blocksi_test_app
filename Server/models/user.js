60633a9079cab70cd6ab6371597bd60cff3e6012
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-26 23:14:29 +0100

    Added starting user model

---
+++ Server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    contacts:[{
+        type: ObjectId,
+        ref: 'Contact'
+    }],
+    token:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:Number,
+        required:true,
+        default:0
+    }
+})
+
+userSchema.plugin(uniqueValidator)
+mongoose.model("User",userSchema)
