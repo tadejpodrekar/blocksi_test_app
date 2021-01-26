135dd88bc5e0a5d74955acaf2cec50b09cfd1cee
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-27 00:36:36 +0100

    Moved db connection to separate file

--- Server/server.js
+++ Server/server.js
@@ -1,35 +1,21 @@
 require('dotenv').config()
+require('./models/user')
+require('./models/contact')
+require('./config/database')
 const express = require('express')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Cors = require('cors')
 const app = express()
-const API_PORT = process.env.API_PORT || 5000
 
-//----Database connection
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useFindAndModify: false
-})
-const db = mongoose.connection
-db.once('open',()=>{
-    console.log("Connected to database")
-})
-db.on('error',(err)=>{
-    console.log("Error connecting to database. ",err)
-})
-//----
+var corsOptions = {
+    origin: 'http://localhost:5000/',
+    optionsSuccessStatus: 200
+  }
 
-app.use(Cors())
+app.use(Cors(corsOptions))
 app.use(bodyParser.json())
+app.use('/register', require('./routes/registration'))
 
-app.get('/', (req, res) => {
-    res.json({
-        message:"Home page"
-    })
-});
-
-app.listen(API_PORT,()=>{
-    console.log("Server is running on port", API_PORT)
+app.listen(process.env.API_PORT,()=>{
+    console.log("Server is running on port", process.env.API_PORT)
 })
