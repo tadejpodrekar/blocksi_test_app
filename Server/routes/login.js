3e294e963cbb2d77b2093ad498178f05683a6414
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-28 00:27:09 +0100

    Working login

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,28 +1,44 @@
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+router.post('/', async (req, res) => {
+    const { username, password } = req.body
 
     try
     {
-        const user = await User.find(u => { return u.username === username && u.password === password });
-
-        if(user)
+        const user = await User.findOne({username:username})
+        if(user && user!=[])
         {
-            const accessToken = jwt.sign({ username: user.username,  role: user.role }, process.env.JWT_SECRET);
-
-            res.status(200).json({accessToken});
+            const match = await bcrypt.compare(password, user.password)
+            console.log(match)
+            if(match)
+            {
+                const token = jwt.sign({ id:user._id }, process.env.JWT_SECRET)
+                res.status(200).json({
+                    auth: true,
+                    accessToken: token,
+                    message: 'Successfully logged in'
+                })
+            }
+            else
+            {
+                console.log('passwords do not match')
+                res.status(400).json({ message:'Username or password incorrect' })
+            }
         }
         else
         {
-            res.send('Username or password incorrect');
+            console.log('user not found')
+            res.status(400).json({ message:'Username or password incorrect' })
         }
     }
     catch(err)
     {
+        console.log(err.message)
         res.status(400).json({message:err.message})
     }
 })
