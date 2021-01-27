866d5a55f2428b8a371be703468f0b739d3d741f
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-28 00:41:23 +0100

    Fixed missing token case

--- Server/middleware/authUser.js
+++ Server/middleware/authUser.js
@@ -1,19 +1,23 @@
 const jwt = require('jsonwebtoken')
 
 function authenticateToken(req, res, next) {
-	let authHeader = req.headers['authorization']
-	let token = authHeader && authHeader.split(' ')[1]
-	if (token == null) return res.sendStatus(401)
-
-	jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if(err)
-        {
-			console.log(err, 'e-authT')
-			return res.sendStatus(403)
-		} 
-		req.user = user
-		next()
-	})
+	const token = req.headers['x-access-token']
+	if (!token)
+	{
+		return res.sendStatus(401)
+	}
+	else
+	{
+		jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+			if(err)
+			{
+				console.log(err, 'e-authT')
+				return res.sendStatus(403)
+			}
+			req.user = user
+			next()
+		})
+	}
 }
 
 module.exports = authenticateToken
