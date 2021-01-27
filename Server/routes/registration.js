fbdd86e37898b857e063d755d2a76e53dde55d86
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-28 00:27:27 +0100

    Working registration

--- Server/routes/registration.js
+++ Server/routes/registration.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
 
-router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
+router.post('/', async (req, res) => {
+    const { username, password } = req.body;
 
-    const user = new User({ username, email, password })
+    const hashedPass = await bcrypt.hash(password, 12)
+    const user = new User({ username, password:hashedPass })
     try
     {
         const newUser = await user.save()
-        res.status(201).json(newUser)
+        const token = jwt.sign({ id:newUser._id }, process.env.JWT_SECRET)
+
+        res.status(201).json({
+            auth: true,
+            accessToken: token,
+            message: 'Successfully registered user'
+        })
     }
     catch(err)
     {
