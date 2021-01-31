9b238e8852a96fabfe9d9092d7c64875373ec965
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-31 02:57:03 +0100

    Fixed indentation

--- blocksi_app/src/App.vue
+++ blocksi_app/src/App.vue
@@ -1,51 +1,47 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<div class="d-flex align-center">
+				<v-img
+					alt="Vuetify Logo"
+					class="shrink mr-2"
+					contain
+					src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+					transition="scale-transition"
+					width="40"
+				/>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+				<v-img
+					alt="Vuetify Name"
+					class="shrink mt-1 hidden-sm-and-down"
+					contain
+					min-width="100"
+					src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+					width="100"
+				/>
+			</div>
 
-      <v-spacer></v-spacer>
-    </v-app-bar>
+			<v-spacer></v-spacer>
+		</v-app-bar>
 
-    <v-main>
-      <Entry/>
-    </v-main>
-  </v-app>
+		<v-main>
+			<Entry/>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
 import Entry from './components/Entry';
 
 export default {
-  name: 'App',
+	name: 'App',
 
-  components: {
-    Entry,
-  },
+	components: {
+	Entry
+	},
 
-  data: () => ({
-    //
-  }),
+	data: () => ({
+	//
+	}),
 };
 </script>
