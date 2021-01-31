a8e590702f09c664d8c985ca314f58f60d43d2f3
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-31 01:14:01 +0100

    Changed main component

--- blocksi_app/src/App.vue
+++ blocksi_app/src/App.vue
@@ -26,31 +26,22 @@
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Entry/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Entry from './components/Entry';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Entry,
   },
 
   data: () => ({
