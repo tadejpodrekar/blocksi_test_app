710910fa4867e801bf67ceb8e3581420f4c01400
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-01-31 02:57:20 +0100

    Added contacts page template

---
+++ blocksi_app/src/components/Contacts.vue
@@ -0,0 +1,42 @@
+<template>
+	<v-app id="contacts">
+		<v-app-bar app shrink-on-scroll>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>Application</v-toolbar-title>
+
+			<v-spacer></v-spacer>
+
+			<v-btn icon>
+				<v-icon>mdi-dots-vertical</v-icon>
+			</v-btn>
+		</v-app-bar>
+
+		<v-main>
+			<v-container>
+				<v-row>
+					<v-col v-for="n in 24" :key="n"	cols="4">
+						<v-card height="200"></v-card>
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-main>
+	</v-app>
+</template>
+
+<style>
+	
+</style>
+
+<script>
+var axios = require('axios');
+axios.defaults.baseURL = 'http://localhost:5000/';
+export default {
+	name: 'Contacts',
+	computed: {
+	},
+	methods: {
+	},
+	data: () => ({
+	}),
+};
+</script>
