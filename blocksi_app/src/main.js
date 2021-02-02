e56ec753ea516df20e9bccf0681fe9c24a0d4b5c
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-02-02 12:46:34 +0100

    Added router definition

--- blocksi_app/src/main.js
+++ blocksi_app/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Router from 'vue-router'
+import vuetify from './plugins/vuetify'
+import Entry from './components/Entry.vue'
+import contacts from './components/Contacts.vue'
 
 Vue.config.productionTip = false
+Vue.use(Router)
+
+export default new Router({
+	routes: [
+		{
+			path: '/',
+			name: 'entry',
+			component: Entry
+		},
+		{
+			path: '/contacts',
+			name: 'contacts',
+			component: contacts
+		}
+	]
+})
 
 new Vue({
 	vuetify,
