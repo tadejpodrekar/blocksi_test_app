04052c10adc99eb7f97d2cb1500684d7ced91168
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-02-04 06:00:36 +0100

    Added user editing form

--- blocksi_app/src/components/Contacts.vue
+++ blocksi_app/src/components/Contacts.vue
@@ -28,7 +28,7 @@
 		</v-navigation-drawer>
 		<v-container>
 			<v-row>
-				<v-col v-for="cont in contacts" :key="cont.firstName"	cols="3">
+				<v-col v-for="cont in contacts" :key="cont.firstName" cols="4">
 					<v-card class="mx-auto" max-width="344" outlined>
 						<v-list-item three-line>
 							<v-list-item-content>
@@ -41,7 +41,7 @@
 							</v-list-item-content>
 						</v-list-item>
 						<v-card-actions>
-							<v-btn outlined rounded text>
+							<v-btn outlined rounded text @click="dialog = !dialog, action = 'Edit'">
 								Edit
 							</v-btn>
 						</v-card-actions>
@@ -49,6 +49,40 @@
 				</v-col>
 			</v-row>
 		</v-container>
+		<v-dialog v-model="dialog" max-width="500px">
+			<v-card class="px-4">
+				<v-card-title>
+					{{ action }} user
+				</v-card-title>
+				<v-card-text>
+					<v-form ref="registerForm" lazy-validation>
+						<v-row>
+							<v-col cols="12">
+								<v-text-field v-model="username" :rules="[rules.required]" label="Username" prepend-icon="mdi-account-circle" required></v-text-field>
+							</v-col>
+							<v-col cols="12">
+								<v-text-field v-model="email" :rule="emailRules" label="E-mail" prepend-icon="mdi-email" required></v-text-field>
+							</v-col>
+							<v-col cols="12" sm="6" md="6">
+								<v-text-field v-model="firstName" :rules="[rules.required]" label="First Name" maxlength="20" prepend-icon="mdi-account-circle" required></v-text-field>
+							</v-col>
+							<v-col cols="12" sm="6" md="6">
+								<v-text-field v-model="lastName" :rules="[rules.required]" label="Last Name" maxlength="20" required></v-text-field>
+							</v-col>
+							<v-col cols="12">
+								<v-text-field v-model="phoneNum" :rules="[rules.required]" label="Phone number" prepend-icon="mdi-phone" required></v-text-field>
+							</v-col>
+						</v-row>
+					</v-form>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn text color="primary" @click="validate, action='Add'">
+						Save
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+        </v-dialog>
 	</div>
 </template>
 
@@ -60,7 +94,7 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:5000/'
 axios.defaults.headers.common['x-access-token'] = localStorage.getItem('token')
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 export default {
 	name: 'Contacts',
 	mounted () {
@@ -75,16 +109,32 @@ export default {
 			))
 	},
 	methods: {
+		validate() {
+			this.dialog = !this.dialog
+		}
 	},
 	data: () => ({
-		drawer: true,
+		action: "Add",
+		dialog: false,
 		mini: true,
+		firstName: "",
+		lastName: "",
+		email: "",
+		username: "",
+		phoneNum: "",
 		contacts: [],
         items: [
           { title: 'Contacts', icon: 'mdi-home-city' },
           { title: 'Add contact', icon: 'mdi-account' },
           { title: 'Logout', icon: 'mdi-account-group-outline' }
-        ]
+		],
+		emailRules: [
+			v => !!v || "Required",
+			v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+		],
+		rules: {
+			required: value => !!value || "Required."
+		}
 	}),
 };
 </script>
