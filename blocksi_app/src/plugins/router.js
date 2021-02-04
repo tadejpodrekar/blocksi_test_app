4a797d9183b061f3cc4ca9da82990c1fff59d540
tadejpodrekar <48256808+tadejpodrekar@users.noreply.github.com>
2021-02-04 18:48:23 +0100

    Simplified routing

--- blocksi_app/src/plugins/router.js
+++ blocksi_app/src/plugins/router.js
@@ -2,28 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import entry from '../components/Entry.vue'
 import contacts from '../components/Contacts.vue'
-import contactView from '../components/ContactView.vue'
-import empty from '../components/EmptyRouterView.vue'
 
 Vue.use(Router)
 
 export default new Router({
 	routes: [
 		{ path: '/', name:'Entry', component: entry },
-        { path: '/contacts', component: empty,
-            children: [{
-                    name: 'Contacts',
-                    path: '',
-                    component: contacts
-                }, {
-                    name: 'contactDialog',
-                    path: ':id',
-                    components: {
-                        default: contacts,
-                        dialog: contactView
-                    }
-                }
-            ]
-        }
+        { path: '/contacts', name:'Contacts', component: contacts}
 	]
 })
